75f5e3d8a6d42ab7ae58fe8fea8374098db8b153
Saddala Yogesh <saddalayogesh@gmail.com>
2026-06-06 23:31:23 +0530

    ✅ Add solution for: Day of the Week Finder

---
+++ exercises/DayOfTheWeekFinder.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        switch (n) {
+            case 1:
+                System.out.print("Monday");
+                break;
+            case 2:
+                System.out.print("Tuesday");
+                break;
+            case 3:
+                System.out.print("Wednesday");
+                break;
+            case 4:
+                System.out.print("Thursday");
+                break;
+            case 5:
+                System.out.print("Friday");
+                break;
+            case 6:
+                System.out.print("Saturday");
+                break;
+            case 7:
+                System.out.print("Sunday");
+                break;
+            default:
+                System.out.print("Invalid Input");
+        }
+
+        sc.close();
+    }
+}
